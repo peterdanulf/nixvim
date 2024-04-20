298147b08ec8e1e18f41e8f6a0202ce9151ffc09
Peter Danulf <peter@MacBook-Pro-som-tillhor-Peter.local>
2024-04-20 08:32:48 +0200

    update colorscheme

--- config/plug/colorscheme/colorscheme.nix
+++ config/plug/colorscheme/colorscheme.nix
@@ -77,46 +77,5 @@ in {
         };
       };
     };
-    catppuccin = {
-      enable = false;
-      settings = {
-        noUnderline = false;
-        noItalic = false;
-        noBold = false;
-        flavour = "frappe"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
-        background = {
-          light = "macchiato";
-          dark = "mocha";
-        };
-        transparentBackground = true;
-        integrations = {
-          cmp = true;
-          noice = true;
-          notify = true;
-          gitsigns = true;
-          which_key = true;
-          illuminate = {
-            enabled = true;
-          };
-          treesitter = true;
-          treesitter_context = true;
-          telescope.enabled = true;
-          indent_blankline.enabled = true;
-          mini.enabled = true;
-          native_lsp = {
-            enabled = true;
-            inlay_hints = {
-              background = true;
-            };
-            underlines = {
-              errors = ["underline"];
-              hints = ["underline"];
-              information = ["underline"];
-              warnings = ["underline"];
-            };
-          };
-        };
-      };
-    };
   };
 }
