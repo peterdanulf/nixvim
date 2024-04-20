555047bec7aa7240e04cd8eb5f512d7478b22f21
Peter Danulf <peter@MacBook-Pro-som-tillhor-Peter.local>
2024-04-20 08:38:08 +0200

    update colorscheme

--- config/plug/colorscheme/colorscheme.nix
+++ config/plug/colorscheme/colorscheme.nix
@@ -56,11 +56,8 @@ in {
     rose-pine = {
       enable = false;
       settings = {
-        darkVariant = "moon";
-        enable = {
-          transparent = true;
-          italic = false;
-        };
+        style = "moon"; #  "main", "moon", "dawn" or raw lua code
+        transparentBackground = true;
         highlightGroups = {
           TelescopeNormal = {
             bg = lua "require('rose-pine.palette').base";
@@ -73,6 +70,47 @@ in {
           TelescopePreviewTitle = {
             bg = lua "require('rose-pine.palette').base";
             fg = lua "require('rose-pine.palette').surface";
+          };
+        };
+      };
+    };
+    catppuccin = {
+      enable = false;
+      settings = {
+        background = {
+          light = "macchiato";
+          dark = "mocha";
+        };
+        flavour = "frappe"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
+        disableBold = false;
+        disableItalic = false;
+        disableUnderline = false;
+        transparentBackground = true;
+        integrations = {
+          cmp = true;
+          noice = true;
+          notify = true;
+          gitsigns = true;
+          which_key = true;
+          illuminate = {
+            enabled = true;
+          };
+          treesitter = true;
+          treesitter_context = true;
+          telescope.enabled = true;
+          indent_blankline.enabled = true;
+          mini.enabled = true;
+          native_lsp = {
+            enabled = true;
+            inlay_hints = {
+              background = true;
+            };
+            underlines = {
+              errors = ["underline"];
+              hints = ["underline"];
+              information = ["underline"];
+              warnings = ["underline"];
+            };
           };
         };
       };
