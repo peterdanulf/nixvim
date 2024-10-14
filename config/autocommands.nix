6597711da6161471e6762d50dc725b88e1f1e9ea
Peter Danulf <peter.danulf@skippo.io>
2024-10-14 12:42:48 +0200

    remove annoying autocommands

--- config/autocommands.nix
+++ config/autocommands.nix
@@ -1,24 +1,5 @@
 {
   autoCmd = [
-    # Vertically center document when entering insert mode
-    {
-      event = "InsertEnter";
-      command = "norm zz";
-    }
-    # Close Telescope prompt in insert mode by clicking escape
-    {
-      event = ["FileType"];
-      pattern = "TelescopePrompt";
-      command = "inoremap <buffer><silent> <ESC> <ESC>:close!<CR>";
-    }
-    # Enable spellcheck for some filetypes
-    {
-      event = "FileType";
-      pattern = [
-        "markdown"
-      ];
-      command = "setlocal spell spelllang=en";
-    }
     # Hilight yank text
     {
       event = "TextYankPost";
